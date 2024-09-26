dce705b636f491b47191ebea2d9ceb150dd4733d
shmackstyle <programmer.shmakov@gmail.com>
2024-09-26 14:38:56 +0300

    Feature-Sliced Design - Architectural methodology

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,13 +1,15 @@
 import React, { FC, useMemo, useState } from 'react'
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext'
+import {
+  LOCAL_STORAGE_THEME_KEY,
+  Theme,
+  ThemeContext,
+} from '../lib/ThemeContext'
 
 const defaultTheme =
   (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT
 
-const ThemeProvider: FC = ({children}) => {
+const ThemeProvider: FC = ({ children }) => {
   const [theme, setTheme] = useState<Theme>(defaultTheme)
-
-
 
   const defaultProps = useMemo(
     () => ({
@@ -17,9 +19,11 @@ const ThemeProvider: FC = ({children}) => {
     [theme]
   )
 
-  return <ThemeContext.Provider value={defaultProps}>
-    {children}
-  </ThemeContext.Provider>
+  return (
+    <ThemeContext.Provider value={defaultProps}>
+      {children}
+    </ThemeContext.Provider>
+  )
 }
 
 export default ThemeProvider
