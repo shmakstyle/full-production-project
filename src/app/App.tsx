b10d89986cb858e66a96b302e2b77bbf8ed26ff3
shmackstyle <programmer.shmakov@gmail.com>
2024-09-28 19:24:27 +0300

    Navbar, First UI Kit component.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
 import './styles/index.scss'
-import { Link, Route, Routes } from 'react-router-dom'
 import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { AppRouter } from './providers/router'
+import { Navbar } from 'widgets/Navbar'
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
   const bool = true
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/'}>Главная страница</Link>
-      <Link to={'/about'}>О сайте</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   )
 }
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -2,12 +2,12 @@ type Mods = Record<string, boolean | string>
 
 export function classNames(
   cls: string,
-  mods: Mods,
-  additional: string[]
+  mods: Mods = {},
+  additional: string[] = []
 ): string {
   return [
     cls,
-    ...additional,
+    ...additional.filter(Boolean),
     ...Object.entries(mods)
       .filter(([className, value]) => Boolean(value))
       .map(([className, value]) => className),
@@ -17,3 +17,4 @@ export function classNames(
 classNames('remove-btn', { hovered: false, selectable: true, red: true }, [
   'pdg',
 ])
+
