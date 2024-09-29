72c0a02b1636671e187280d44afb64d18e5643b0
shmackstyle <programmer.shmakov@gmail.com>
2024-09-29 21:51:05 +0300

    3 Svg loader, File loader, Button UI kit

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,20 @@ import webpack from 'webpack'
 import { BuildOptions } from './types/config'
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  }
+
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  }
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -12,7 +26,9 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         options: {
           modules: {
             auto: (resPath: string) => Boolean(resPath.includes('module.')),
-            localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]',
+            localIdentName: isDev
+              ? '[path][name]__[local]--[hash:base64:5]'
+              : '[hash:base64:8]',
           },
         },
       },
@@ -27,5 +43,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   }
 
-  return [typescriptLoader, cssLoader]
+  return [typescriptLoader, cssLoader, fileLoader, svgLoader]
 }
--- src/app/providers/ThemeProvider/index.tsx
+++ src/app/providers/ThemeProvider/index.tsx
@@ -1,5 +1,5 @@
-import { useTheme } from './lib/useTheme'
 import ThemeProvider from './ui/ThemeProvider'
+import {Theme} from './lib/ThemeContext'
+import { useTheme } from './lib/useTheme'
 
-
-export { ThemeProvider, useTheme }
+export { ThemeProvider, useTheme, Theme}
