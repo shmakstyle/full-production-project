80e6253076e00afaeacecb5f0a966363635c3a73
shmackstyle <programmer.shmakov@gmail.com>
2024-09-30 23:03:17 +0300

    Sidebar. Layout app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,8 @@ import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { AppRouter } from './providers/router'
 import { Navbar } from 'widgets/Navbar'
+import { Sidebar } from 'widgets/Sidebar'
+
 
 const App = () => {
   const { theme } = useTheme()
@@ -10,7 +12,10 @@ const App = () => {
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
-      <AppRouter />
+      <div className='content-page'>
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   )
 }
